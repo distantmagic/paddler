d7a9ec41d3f8b72028fd0baac46a3aaf8eec636d
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-11-15 12:59:13 +0100

    chore: pingora proxy

--- src/cmd/balancer.rs
+++ src/cmd/balancer.rs
@@ -1,9 +1,11 @@
 use actix_web::{App, HttpServer};
 use pingora::prelude::*;
-use std::net::SocketAddr;
+use pingora::upstreams::peer::HttpPeer;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::task;
 
 use crate::balancer::http_route;
+use crate::cmd::proxy::ProxyApp;
 use crate::errors::result::Result;
 
 pub async fn handle(management_addr: &SocketAddr, reverseproxy_addr: &SocketAddr) -> Result<()> {
@@ -16,9 +18,12 @@ pub async fn handle(management_addr: &SocketAddr, reverseproxy_addr: &SocketAddr
 
     join_set.spawn(management_server);
     join_set.spawn(async move {
-        let mut my_server = Server::new(None).unwrap();
-        my_server.bootstrap();
-        my_server.run_forever();
+        let peer = HttpPeer::new(
+            "127.0.0.1:8080".parse::<SocketAddr>().unwrap(),
+            false,
+            "".to_string(),
+        );
+        let proxy_app = ProxyApp::new(peer);
 
         Ok(())
     });
