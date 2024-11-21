3f9b1731e8e0dd73a90b2696e8b6ba7428d39a38
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-11-21 11:29:04 +0100

    chore: use Drop guard to drop agent

--- src/balancer/http_route/receive_status_update.rs
+++ src/balancer/http_route/receive_status_update.rs
@@ -1,6 +1,6 @@
 use actix_web::{post, web, Error, HttpResponse};
 use futures_util::StreamExt as _;
-use log::error;
+use log::{error, info};
 use serde::Deserialize;
 
 use crate::balancer::status_update::StatusUpdate;
@@ -15,12 +15,34 @@ struct PathParams {
     agent_id: String,
 }
 
+struct RemovePeerGuard<'a> {
+    pool: &'a UpstreamPeerPool,
+    agent_id: String,
+}
+
+impl<'a> Drop for RemovePeerGuard<'a> {
+    fn drop(&mut self) {
+        info!("Removing agent: {}", self.agent_id);
+
+        if let Err(e) = self.pool.remove_peer(&self.agent_id) {
+            error!("Failed to remove peer: {}", e);
+        }
+    }
+}
+
 #[post("/status_update/{agent_id}")]
 async fn respond(
     path_params: web::Path<PathParams>,
     mut payload: web::Payload,
     upstream_peer_pool: web::Data<UpstreamPeerPool>,
 ) -> Result<HttpResponse, Error> {
+    let _guard = RemovePeerGuard {
+        pool: &upstream_peer_pool,
+        agent_id: path_params.agent_id.clone(),
+    };
+
+    info!("Registering agent: {}", path_params.agent_id);
+
     while let Some(chunk) = payload.next().await {
         match serde_json::from_slice::<StatusUpdate>(&chunk?) {
             Ok(status_update) => {
@@ -33,14 +55,12 @@ async fn respond(
                 }
             }
             Err(e) => {
-                error!("Failed toarse status update: {}", e);
+                error!("Failed to parse status update: {}", e);
 
                 return Err(Error::from(e));
             }
         }
     }
-
-    upstream_peer_pool.remove_peer(&path_params.agent_id)?;
 
     Ok(HttpResponse::Accepted().finish())
 }
