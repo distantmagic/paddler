897936b17adf876b16e1c36fac3ef388dad3906f
Propfend <mellomello2030@gmail.com>
2024-12-30 11:36:49 +0000

    detaching work on windows.

--- src/supervisor/applying_service.rs
+++ src/supervisor/applying_service.rs
@@ -58,40 +58,33 @@ impl ApplyingService {
     }
 
     async fn start_llamacpp_server(&mut self) -> Result<()> {
-        unsafe {
-            let mut cmd = Command::new(self.llama_path.to_owned());
+        let mut cmd = Command::new(self.llama_path.to_owned());
 
-            if !is_a_gguf_file(self.model_path.to_string()) {
-                return Err(AppError::InvalidFileError(
-                    "Insert a an existent gguf file for a model.".to_owned(),
-                ));
-            }
-
-            let port = get_port(self.addr.clone());
-            let host = get_host(self.addr.clone());
-
-            cmd.args(&[
-                "-m",
-                self.model_path.as_str(),
-                "--host",
-                &host,
-                "--port",
-                &port,
-                "--slots",
-            ])
-            .stdout(Stdio::null())
-            .stderr(Stdio::null());
-
-            #[cfg(unix)]
-            cmd.pre_exec(|| {
-                libc::setsid();
-
-                Ok(())
-            });
-
-            let child = cmd.spawn()?;
-            self.llama_process = Some(child);
+        if !is_a_gguf_file(self.model_path.to_string()) {
+            return Err(AppError::InvalidFileError(
+                "Insert a an existent gguf file for a model.".to_owned(),
+            ));
         }
+
+        let port = get_port(self.addr.clone());
+        let host = get_host(self.addr.clone());
+
+        cmd.args(&[
+            "-m",
+            self.model_path.as_str(),
+            "--host",
+            &host,
+            "--port",
+            &port,
+            "--slots",
+        ])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null());
+
+        detach_process(&mut cmd);
+
+        let child = cmd.spawn()?;
+        self.llama_process = Some(child);
 
         Ok(())
     }
@@ -122,6 +115,8 @@ impl Service for ApplyingService {
         let mut ticker = interval(self.monitoring_interval);
         ticker.set_missed_tick_behavior(MissedTickBehavior::Delay);
 
+        // Kill old running llamacpp if there is one
+
         unsafe {
             if let Ok(listeners) = listeners::get_processes_by_port(
                 str::parse(get_port(self.addr.clone()).as_str()).unwrap_unchecked(),
@@ -132,6 +127,7 @@ impl Service for ApplyingService {
             }
         }
 
+        // Start a new llamacpp so we can have control over this child
         match self.start_llamacpp_server().await {
             Err(e) => {
                 warn!(
@@ -190,7 +186,6 @@ impl Service for ApplyingService {
                             if let Some(llama_process) = &mut self.llama_process {
                                 let pid = llama_process.id();
 
-                                #[cfg(unix)]
                                 kill_llamacpp_server(pid);
 
                                 self.addr = addr;
@@ -259,6 +254,23 @@ fn kill_llamacpp_server(pid: u32) {
     unsafe {
         if libc::kill(-(pid as i32), libc::SIGKILL) != 0 {
             warn!("Failed to kill process group. Changes were not applied");
+        }
+    }
+}
+
+fn detach_process(cmd: &mut Command) {
+    unsafe {
+        #[cfg(unix)]
+        cmd.pre_exec(|| {
+            libc::setsid();
+
+            Ok(())
+        });
+
+        #[cfg(target_family = "windows")]
+        {
+            const DETACHED_PROCESS: u32 = 0x00000008;
+            cmd.creation_flags(DETACHED_PROCESS);
         }
     }
 }
