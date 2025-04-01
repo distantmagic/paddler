b9c813614b06999f23657d43648108e2f2c381a3
Propfend <mellomello2030@gmail.com>
2025-04-01 18:30:28 +0000

    unit tests and integration works.

--- src/tests/unit/llamacpp_client.rs
+++ src/tests/unit/llamacpp_client.rs
@@ -1,42 +1,13 @@
-use cucumber::{given, then, when, World};
 use httpmock::{Method::GET, MockServer};
 use serde_json::json;
 
-use crate::{
-    errors::{app_error::AppError, result::Result},
-    llamacpp::{llamacpp_client::LlamacppClient, slots_response::SlotsResponse},
-};
+use crate::{errors::result::Result, llamacpp::llamacpp_client::LlamacppClient};
 
-#[derive(Default)]
-struct Mock(Option<MockServer>);
-
-impl std::fmt::Debug for Mock {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "MockServer")
-    }
-}
-
-#[derive(Debug, Default, cucumber::World)]
-struct LlamacppClientWorld {
-    pub mock: Mock,
-    pub response: SlotsResponse,
-    pub error: Option<AppError>,
-    pub client: LlamacppClient,
-}
-
-#[given(regex = r"llamacpp server is running")]
-async fn setup_llamacpp_server(world: &mut LlamacppClientWorld) -> Result<()> {
+#[tokio::test]
+async fn slots_are_authorized() -> Result<()> {
     let mock_server = MockServer::start();
-    world.client = LlamacppClient::new(*mock_server.address(), None)?;
+    let client = LlamacppClient::new(*mock_server.address(), None)?;
 
-    world.mock.0 = Some(mock_server);
-
-    Ok(())
-}
-
-#[when("llamacpp client requests available slots endpoint with an authorized slots response")]
-async fn request_slots_success(world: &mut LlamacppClientWorld) -> Result<()> {
-    let mock_server = world.mock.0.as_ref().unwrap();
     let _mock = mock_server.mock(|when, then| {
         when.method(GET).path("/slots");
         then.status(200)
@@ -50,23 +21,20 @@ async fn request_slots_success(world: &mut LlamacppClientWorld) -> Result<()> {
             ]));
     });
 
-    world.response = world.client.get_available_slots().await?;
-    world.error = None;
+    let response = client.get_available_slots().await?;
+
+    assert!(response.is_authorized.unwrap());
+    assert!(response.is_slot_endpoint_enabled.unwrap());
+    assert!(!response.slots.is_empty());
 
     Ok(())
 }
 
-#[then("llamacpp client must receive a successful response with slots")]
-async fn verify_successful_response(world: &mut LlamacppClientWorld) {
-    let response = &world.response;
-    assert!(response.is_authorized.unwrap());
-    assert!(response.is_slot_endpoint_enabled.unwrap());
-    assert!(!response.slots.is_empty());
-}
+#[tokio::test]
+async fn slots_are_unauthorized() -> Result<()> {
+    let mock_server = MockServer::start();
+    let client = LlamacppClient::new(*mock_server.address(), None)?;
 
-#[when("llamacpp client requests available slots endpoint with an unauthorized slots response")]
-async fn request_slots_failure(world: &mut LlamacppClientWorld) -> Result<()> {
-    let mock_server = world.mock.0.as_ref().unwrap();
     let _mock = mock_server.mock(|when, then| {
         when.method(GET).path("/slots");
         then.status(401)
@@ -80,24 +48,20 @@ async fn request_slots_failure(world: &mut LlamacppClientWorld) -> Result<()> {
             ]));
     });
 
-    world.response = world.client.get_available_slots().await?;
-    world.error = None;
-
-    Ok(())
-}
-
-#[then("llamacpp client must receive an unauthorized response")]
-async fn verify_unauthorized_response(world: &mut LlamacppClientWorld) {
-    let response = &world.response;
+    let response = client.get_available_slots().await?;
 
     assert!(!response.is_authorized.unwrap());
     assert!(response.is_slot_endpoint_enabled.is_none());
     assert!(response.slots.is_empty());
+
+    Ok(())
 }
 
-#[when("llamacpp client requests available slots endpoint with an not implemented response")]
-async fn request_slots_unimplemented(world: &mut LlamacppClientWorld) -> Result<()> {
-    let mock_server = world.mock.0.as_ref().unwrap();
+#[tokio::test]
+async fn response_is_unimplemented() -> Result<()> {
+    let mock_server = MockServer::start();
+    let client = LlamacppClient::new(*mock_server.address(), None)?;
+
     let _mock = mock_server.mock(|when, then| {
         when.method(GET).path("/slots");
         then.status(501)
@@ -111,41 +75,28 @@ async fn request_slots_unimplemented(world: &mut LlamacppClientWorld) -> Result<
             ]));
     });
 
-    world.response = world.client.get_available_slots().await?;
-    world.error = None;
+    let response = client.get_available_slots().await?;
+
+    assert!(response.is_authorized.is_none());
+    assert!(!response.is_slot_endpoint_enabled.unwrap());
+    assert!(response.slots.is_empty());
 
     Ok(())
 }
 
-#[then("llamacpp client must receive an unimplemented response")]
-async fn verify_unimplemented_response(world: &mut LlamacppClientWorld) {
-    let response = &world.response;
-    assert!(response.is_authorized.is_none());
-    assert!(!response.is_slot_endpoint_enabled.unwrap());
-    assert!(response.slots.is_empty());
-}
-
-#[when("llamacpp client requests available slots endpoint with an error response")]
-async fn request_slots_error(world: &mut LlamacppClientWorld) -> Result<()> {
-    let mock_server = world.mock.0.as_ref().unwrap();
+#[tokio::test]
+async fn response_is_error() -> Result<()> {
+    let mock_server = MockServer::start();
+    let client = LlamacppClient::new(*mock_server.address(), None)?;
 
     let _mock = mock_server.mock(|when, then| {
         when.method(GET).path("/slots");
         then.status(99);
     });
 
-    world.error = world.client.get_available_slots().await.err();
+    let error = client.get_available_slots().await.err();
+
+    assert!(error.is_some());
 
     Ok(())
-}
-
-#[then("llamacpp client must receive an error response")]
-async fn verify_error_response(world: &mut LlamacppClientWorld) {
-    assert!(world.error.is_some());
-}
-
-#[cfg(not(miri))]
-#[tokio::test]
-async fn run_cucumber_tests() {
-    LlamacppClientWorld::run("src/tests/features/llamacpp_client.feature").await;
 }
