42d5875cbded4b3b64d613d34b09a41a61a2d15d
Propfend <mellomello2030@gmail.com>
2025-06-11 19:41:23 +0000

    llamacpp mock is incrementing and decrementing the number of slots processing and idle.

--- integration_tests/tests/fixtures/llamacpp-server-mock.mjs
+++ integration_tests/tests/fixtures/llamacpp-server-mock.mjs
@@ -3,6 +3,17 @@
 import { createServer } from 'node:http';
 import { parseArgs } from 'node:util';
 
+function toggle_value(array, toggle_value) {
+  const indexes = array
+    .map((val, i) => val === toggle_value ? i : -1)
+    .filter(i => i !== -1);
+
+  if (indexes.lenght === 0) return;
+
+  const randomIndex = indexes[Math.floor(Math.random() * indexes.length)];
+  array[randomIndex] = !array[randomIndex];
+}
+
 const {
   values: {
     port,
@@ -20,28 +31,40 @@ const {
   },
 });
 
-const slotsInt = parseInt(slots, 10);
-const slotsStatuses = [];
-
-for (let i = 0; i < slotsInt; i += 1) {
-  slotsStatuses.push({
-    id: i,
-    is_processing: false,
-  });
+let slotsNum = [];
+for (let i = 0; i < parseInt(slots); i++) {
+  slotsNum.push(false);
 }
 
-const server = createServer(function (req, res) {
+const server = createServer(async function (req, res) {
   console.log('Request:', req.method, req.url);
 
   if (req.url === "/chat/completions") {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('{}');
+    if (slots.length <= 0) {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ status: "no slots available" }));
+    } else {
+      toggle_value(slotsNum, false);
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      toggle_value(slotsNum, true);  
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end('{}');
+    }    
   } else if (req.url === '/health') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('OK');
   } else if (req.url === '/slots') {
+    const slotsStatuses = [];
+
+    for (let i = 0; i < slotsNum.length; i += 1) {
+      slotsStatuses.push({
+        id: i,
+        is_processing: slotsNum[i],
+      });
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(slotsStatuses));
