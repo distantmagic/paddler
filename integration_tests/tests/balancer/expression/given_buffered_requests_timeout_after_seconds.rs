22d6e4bce36b154004be4565c1e5d67cfd618382
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-06-24 16:42:39 +0200

    make expression names match their filenames

--- integration_tests/tests/balancer/expression/given_buffered_requests_timeout_after_seconds.rs
+++ integration_tests/tests/balancer/expression/given_buffered_requests_timeout_after_seconds.rs
@@ -4,7 +4,7 @@ use cucumber::given;
 use crate::paddler_world::PaddlerWorld;
 
 #[given(expr = "buffered requests timeout after {int} second(s)")]
-pub async fn given_agent_is_attached(
+pub async fn given_buffered_requests_timeout_after_seconds(
     world: &mut PaddlerWorld,
     buffered_request_timeout: i64,
 ) -> Result<()> {
