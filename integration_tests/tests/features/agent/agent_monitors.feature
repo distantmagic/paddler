8a44892cc5841517f19ec58b9c0d5d8a5ea11a17
Propfend <mellomello2030@gmail.com>
2025-06-26 14:57:04 +0000

    resolve conflict.

--- integration_tests/tests/features/agent/agent_monitors.feature
+++ integration_tests/tests/features/agent/agent_monitors.feature
@@ -1,14 +1,5 @@
 Feature: Monitor llama.cpp instances
 
-<<<<<<< HEAD
-    Background:
-        Given balancer is running
-        Given llama.cpp server "llama-1" is running (has 1 slot)
-        Given llama.cpp server "llama-2" is running (has 1 slot)
-        Given llama.cpp server "llama-3" is running (has 1 slot)
-
-=======
->>>>>>> 4e5a40cbc4fe507e6b9a5a9ac8695b7087d2ee11
     @serial
     Scenario: Agent attaches llama.cpp
         Given balancer is running
@@ -22,24 +13,6 @@ Feature: Monitor llama.cpp instances
         Given agent "agent-3" is running (observes "llama-3")
         Given agent "agent-3" is registered
         Then balancer state is:
-<<<<<<< HEAD
-        |  agent  | is_connect_error |
-        | agent-1 |      false       |
-        | agent-2 |      false       |
-        | agent-3 |      false       |
-        When llama.cpp server "llama-1" stops running
-        Then next balancer state is:
-        |  agent  | is_connect_error |
-        | agent-1 |      true        |
-        | agent-2 |      false       |
-        | agent-3 |      false       |
-        When llama.cpp server "llama-3" stops running
-        Then next balancer state is:
-        |  agent  | is_connect_error |
-        | agent-1 |      true        |
-        | agent-2 |      false       |
-        | agent-3 |      true        |
-=======
             | agent   | is_connect_error |
             | agent-1 | false            |
             | agent-2 | false            |
@@ -56,4 +29,3 @@ Feature: Monitor llama.cpp instances
             | agent-1 | true             |
             | agent-2 | false            |
             | agent-3 | true             |
->>>>>>> 4e5a40cbc4fe507e6b9a5a9ac8695b7087d2ee11
--- integration_tests/tests/paddler/expression/then_balancer_state_is.rs
+++ integration_tests/tests/paddler/expression/then_balancer_state_is.rs
@@ -9,94 +9,15 @@ use crate::balancer_table::assert_balancer_table;
 use crate::balancer_table::fetch_status;
 use crate::paddler_world::PaddlerWorld;
 
-<<<<<<< HEAD
-async fn fetch_status(balancer_port: u16) -> Result<Response> {
-    let response = reqwest::get(format!("http://127.0.0.1:{balancer_port}/api/v1/agents")).await?;
-    if !response.status().is_success() {
-        return Err(anyhow!(
-            "Dashboard check failed: Expected status 200, got {}",
-            response.status()
-        ));
-    }
-    Ok(response)
-}
-
-fn assert_fields(table_fields: Vec<Option<&String>>, peer_fields: Vec<String>) {
-    for (index, table_field) in table_fields.iter().enumerate() {
-        if let Some(field) = table_field {
-            assert_eq!(**field, peer_fields[index])
-        }
-    }
-}
-
-fn get_agent_last_update(agents_response: &AgentsResponse) -> SystemTime {
-    let mut last_update = SystemTime::now();
-
-    for agent in &agents_response.agents {
-        if agent.last_update > last_update {
-            last_update = agent.last_update;
-        }
-    }
-
-    last_update
-}
-
-=======
->>>>>>> 4e5a40cbc4fe507e6b9a5a9ac8695b7087d2ee11
 #[then("balancer state is:")]
 pub async fn then_balancer_state_is(world: &mut PaddlerWorld, step: &Step) -> Result<()> {
     let response = fetch_status(8095).await?;
     let agents_response = response.json::<AgentsResponse>().await?;
 
-<<<<<<< HEAD
-    let last_agents_update = get_agent_last_update(&agents_response);
-
-    world.last_update = Some(last_agents_update);
-
-    if let Some(table) = step.table.as_ref() {
-        let headers = &table.rows[0];
-
-        for row in table.rows.iter().skip(1) {
-            let agent_name = row
-                .get(0)
-                .ok_or_else(|| anyhow!("Missing agent name in table row"))?;
-
-            let peer = agents_response
-                .agents
-                .iter()
-                .find(|p| &p.status.agent_name == agent_name)
-                .ok_or_else(|| anyhow!("Agent {} not found in response", agent_name))?;
-
-            let mut table_fields = Vec::new();
-            let mut peer_fields = Vec::new();
-
-            for (col_idx, header) in headers.iter().enumerate() {
-                match header.as_str() {
-                    "agent" => {
-                        table_fields.push(row.get(col_idx));
-                        peer_fields.push(peer.status.agent_name.clone());
-                    }
-                    "is_connect_error" => {
-                        table_fields.push(row.get(col_idx));
-                        peer_fields.push(
-                            peer.status
-                                .is_connect_error
-                                .map(|v| v.to_string())
-                                .unwrap_or("None".to_string()),
-                        );
-                    }
-                    _ => continue,
-                }
-            }
-
-            assert_fields(table_fields, peer_fields);
-        }
-=======
     world.last_balancer_state_update = Some(SystemTime::now());
 
     if let Some(table) = step.table.as_ref() {
         assert_balancer_table(table, &agents_response)?;
->>>>>>> 4e5a40cbc4fe507e6b9a5a9ac8695b7087d2ee11
     }
 
     Ok(())
