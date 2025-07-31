71d4b36c80cea22c55fcc953f62a80ba3482aa1b
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-07-31 19:33:58 +0200

    how to add a new agent

--- resources/ts/components/DashboardPage.tsx
+++ resources/ts/components/DashboardPage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { AgentListStream } from "./AgentListStream";
 import { DashboardBufferedRequests } from "./DashboardBufferedRequests";
+import { DashboardPageAddAgent } from "./DashboardPageAddAgent";
 import { DashboardPageAddrOverview } from "./DashboardPageAddrOverview";
 
 import {
   dashboardPage,
+  dashboardPage__blocks,
   dashboardPage__serviceBlock,
 } from "./DashboardPage.module.css";
 
@@ -22,20 +24,23 @@ export function DashboardPage({
 }) {
   return (
     <div className={dashboardPage}>
-      <div className={dashboardPage__serviceBlock}>
-        <DashboardPageAddrOverview
-          inferenceAddr={inferenceAddr}
-          managementAddr={managementAddr}
-        />
-      </div>
-      <div className={dashboardPage__serviceBlock}>
-        <DashboardBufferedRequests
-          bufferedRequestTimeoutMilis={bufferedRequestTimeoutMilis}
-          maxBufferedRequests={maxBufferedRequests}
-        />
-      </div>
-      <div className={dashboardPage__serviceBlock}>
-        <AgentListStream managementAddr={managementAddr} />
+      <div className={dashboardPage__blocks}>
+        <div className={dashboardPage__serviceBlock}>
+          <DashboardPageAddrOverview
+            inferenceAddr={inferenceAddr}
+            managementAddr={managementAddr}
+          />
+        </div>
+        <div className={dashboardPage__serviceBlock}>
+          <DashboardBufferedRequests
+            bufferedRequestTimeoutMilis={bufferedRequestTimeoutMilis}
+            maxBufferedRequests={maxBufferedRequests}
+          />
+        </div>
+        <div className={dashboardPage__serviceBlock}>
+          <AgentListStream managementAddr={managementAddr} />
+          <DashboardPageAddAgent managementAddr={managementAddr} />
+        </div>
       </div>
     </div>
   );
