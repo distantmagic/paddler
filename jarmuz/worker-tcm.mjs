374392328d727972af812557d0420eab9b02477b
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-08-07 15:39:53 +0200

    cleanup old .module.css.d.ts files

--- jarmuz/worker-tcm.mjs
+++ jarmuz/worker-tcm.mjs
@@ -1,10 +1,16 @@
+import { glob } from "glob";
 import { join } from "node:path";
 import { run } from "typed-css-modules";
+import { unlink } from "node:fs/promises";
 
 import { basic } from "jarmuz/job-types";
 
 basic(async function ({ baseDirectory }) {
   let outDir = join(baseDirectory, "resources/ts");
+
+  for (const filepath of await glob("resources/ts/components/**/*.module.css.d.ts")) {
+    await unlink(filepath);
+  }
 
   await run(outDir, {
     pattern: "**/*.module.css",
