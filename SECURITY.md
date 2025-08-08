ab6b29a9011828b3ee2dc7933bdff6a002029068
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-08-08 15:28:27 +0200

    remove outdated docs, update docker example

--- SECURITY.md
+++ SECURITY.md
@@ -1,3 +1,5 @@
-## 🚨 Security Notice
-Thank you for reporting a security issue. **Do NOT disclose sensitive details publicly**.  
-Instead, please email us at **[security@intentt.io](mailto:security@intentt.io)** if necessary.
+## Security Notice
+
+Thank you for reporting a security issue. Please do not disclose sensitive details publicly.
+
+Instead, please email us at **[security@intentee.com](mailto:security@intentee.com)** if necessary.
--- infra/terraform/aws/aws_imagebuilder_image_pipeline_llamacpp_gpu_compute_75.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_imagebuilder_image_pipeline" "llamacpp_gpu_compute_75" {
-  name                             = "llamacpp_gpu_compute_75"
-  image_recipe_arn                 = aws_imagebuilder_image_recipe.llamacpp_gpu_compute_75.arn
-  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.llamacpp_gpu_compute_75.arn
-  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.llamacpp_gpu_compute_75.arn
-  status                           = "ENABLED"
-}
