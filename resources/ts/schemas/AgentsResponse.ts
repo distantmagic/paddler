68c81fda5b2cf69b069bb3514d7d93693ea39277
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-08-08 19:20:46 +0200

    sort agents by name in web admin panel

--- resources/ts/schemas/AgentsResponse.ts
+++ resources/ts/schemas/AgentsResponse.ts
@@ -1,11 +1,18 @@
 import { z } from "zod";
 
-import { AgentSchema } from "./Agent";
+import { AgentSchema, type Agent } from "./Agent";
 
 export const AgentsResponseSchema = z
   .object({
     agents: z.array(AgentSchema),
   })
-  .strict();
+  .strict()
+  .transform(function ({ agents }) {
+    return Object.freeze({
+      agents: agents.sort(function (a: Agent, b: Agent) {
+        return String(a.name).localeCompare(String(b.name));
+      }),
+    });
+  });
 
 export type AgentsResponse = z.infer<typeof AgentsResponseSchema>;
